44d04c538e1649073aa0dee06eefad005bbe2a8f
Admin <Admin@DESKTOP-4ULN1CL>
2024-08-13 21:51:37 +0700

    add hello

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -6,6 +6,7 @@ export default function HomePage() {
     <div>
       <div className=' max-w-screen-cus mx-auto'>
         <DemoComponet/>
+        <div>hell work</div>
       </div>
     </div>
   )
